804d4a0bff823280336f01a73ddadefc91fc60da
unbothered <pauliheabunike@gmail.com>
2026-08-26 03:12:49 +0100

    Add JWT refresh endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 from . import admin_views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('admin/users/', admin_views.AdminUserListView.as_view(), name='admin-users'),
     path('admin/grant-plan/', admin_views.AdminGrantPlanView.as_view(), name='admin-grant-plan'),
     path('me/', views.MeView.as_view(), name='me'),
